3ac6c58e190559f4b0556602b18be56d164bd288
tan <tanda.arch@gmail.com>
2017-12-05 00:58:33 +0800

    常用状态参数

--- Src/Response/ResponseStatus.php
+++ Src/Response/ResponseStatus.php
@@ -6,31 +6,27 @@ use ArrayIterator;
 use Countable;
 use InvalidArgumentException;
 
-class ResponseStatus implements Countable, IteratorAggregate
-{
+class ResponseStatus implements Countable, IteratorAggregate {
     /**
-     * Create a new Httpstatus Instance.
+     * Create a new Response Instance.
      *
      * @throws InvalidArgumentException if the collection is not valid
      */
-    public function __construct($statusArray = [])
-    {
+    public function __construct($statusArray = []) {
 
     }
 
     /**
      * {@inheritdoc}
      */
-    public function count()
-    {
-        return count($this->httpStatus);
+    public function count() {
+        return count($this->responseStatus);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getIterator()
-    {
-        return new ArrayIterator($this->httpStatus);
+    public function getIterator() {
+        return new ArrayIterator($this->responseStatus);
     }
 }
---
+++ Src/Response/Responsecodes.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tanel\Http;
+
+interface ResponseCodes {
+    /**
+     * Response code as a constant.
+     * 1xxx 参数异常
+     * 2xxx 操作成功
+     * 3xxx 操作失败
+     * 4xxx 用户异常
+     * 5xxx 服务状态
+     * 6xxx 接口状态
+     */
+    const PM_INVALID         = 1000; //参数无法通过验证
+    const PM_NOT_ALLOW_EMPTY = 1001; //参数不允许为空
+    const PM_MISSING         = 1002; //缺少必要参数
+    const PM_TYPE_MISMATCH   = 1003; //参数类型不匹配
+    const PM_REQUIRE_INT     = 1004; //需要integer参数
+    const PM_REQUIRE_STRING  = 1005; //需要string类型参数
+    const PM_REQUIRE_ARRAY   = 1006; //需要array类型参数
+
+    //2xxx
+    const SUCC_INSERT  = 2001; //插入成功
+    const SUCC_UPDATE  = 2002; //更新成功
+    const SUCC_CHANGE  = 2003; //修改成功
+    const SUCC_REPLACE = 2004; //删除成功
+    const SUCC_DELETE  = 2005; //删除成功
+    const SUCC_REMOVE  = 2005; //移除成功
+
+    //3xxx
+    const FAIL_INSERT  = 3001; //插入失败
+    const FAIL_UPDATE  = 3002; //更新失败
+    const FAIL_CHANGE  = 3003; //修改失败
+    const FAIL_REPLACE = 3004; //删除失败
+    const FAIL_DELETE  = 3005; //删除失败
+    const FAIL_REMOVE  = 3006; //移除失败
+
+    //4xxx
+    const USER_EXCEPTION    = 4000; //用户态异常
+    const USER_NOT_FOUND    = 4001; //用户不存在
+    const USER_NOT_LOGININ  = 4002; //用户未登录
+    const USER_PASSWD_ERROR = 4003; //密码错误
+    const USER_IS_EXIST     = 4004; //用户已存在
+    const USER_TOKEN_ERROR  = 4005; //用户授权口令错误
+    const USER_NO_AUTH      = 4006; //用户没用权限
+
+    //5xxx
+    const SERVER_EXCEPTION = 5000; //服务器异常
+    const SERVER_TIMEOUT   = 5001;
+    const SERVER_OUT_LIMIT = 5003;
+
+    //6xxx
+    const API_EXCEPTION    = 6000; //接口异常
+    const API_NOT_FOUND    = 6001; //接口不存在
+    const API_METHOD_ERROR = 6002; //接口请求方式错误
+    const API_VERSION_EROR = 6003; //版本不支持
+    const API_TIMEOUT      = 6004; //接口超时
+
+}
